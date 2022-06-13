0376b6f792756ebc03ce6a191719b0a523676aee
Vulosophy <72704597+Vulosophy@users.noreply.github.com>
2022-06-13 17:04:06 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+/*var output = document.querySelector("p")
+output.innerHTML = "Hello World!";
+output.style.color = "blue";
+output.style.fontSize = "50px";
+//output.style.textDecoration = "underline";
+output.style.marginLeft ="1080px";*/
+
+
+
+/*var button = document.querySelector("button");
+button.addEventListener("click", clickHandler, false);
+button.className = "pointer";
+var wait = document.querySelector("button");
+wait.className = "wait";
+var move = document.querySelector("move");
+var help = document.querySelector("help");
+var crosshair = document.querySelector("crosshair");*/
+
+/*button.style.marginTop = "250px";
+button.style.marginLeft = "1080px";
+input.style.marginTop = "250px";
+input.style.marginLeft = "820px";*/
+function clickHandler() {
+    //console.log("You Clicked The Button");
+    //button.removeEventListener("click", clickHandler, false);
+    //button.style.display = "none";
+    //score += 2;
+    //console.log(score);
+    var input = document.querySelector("#input");
+    var output = documentquerySelector("#output");
+    output.innerHTML = input.value;
+}
+
+/* var score = 0
+console.log(score);
+
+window.addEventListener("keydown",keydownHandler, false);
+function keydownHandler(event)
+{
+    if(event.keyCode === 13)
+    {
+        console.log("Enter key pressed")
+    }
+}
+add(3, 4);
+subtract(10, 4);
+divide(225, 5);
+multiply(43, 2);
+function add(firstNumber, secondNumber)
+{
+    var total = firstNumber + secondNumber
+    console.log(firstNumber + " + " + secondNumber + " = " + total);
+}
+function subtract(firstNumber, secondNumber)
+{
+    var difference = firstNumber - secondNumber
+    console.log(firstNumber + " - " + secondNumber + " = " + difference);
+}
+function divide(firstNumber, secondNumber)
+{
+    var quotient = firstNumber / secondNumber
+    console.log(firstNumber + "/" + secondNumber + " = " + quotient);
+} 
+function multiply(firstNumber, secondNumber)
+{
+    var product = firstNumber * secondNumber
+    console.log(firstNumber + " * " + secondNumber + " = " + product);
+} 
+*/
